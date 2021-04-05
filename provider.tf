b1774596aa45893e3ba360bdb223b61a04bfcd1e
Stee-maker <59018335+Stee-maker@users.noreply.github.com>
2021-04-05 16:02:37 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,12 +3,11 @@ required_providers {
 openstack = {
 source = "terraform-provider-openstack/openstack"
 version = "1.40.0"
-} }
-
+} 
 } 
   }
-}
 provider "openstack" {
 auth_url = "https://auth.cloud.ovh.net/v3/"
 alias = "ovh"
 region = var.region
+  }
