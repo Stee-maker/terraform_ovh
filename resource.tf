5f0ff540e4acd7846e6566f8f87d15c71a8d310d
Stee-maker <59018335+Stee-maker@users.noreply.github.com>
2021-04-05 16:18:51 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -5,7 +5,7 @@ public_key = var.mysecret_key
 }
 resource "openstack_compute_instance_v2" "terraform_instance_demo" {
 provider = openstack.ovh
-count = 1
+count = var.server_web
 name = "terraform_instance_demo"
 image_name = var.image_name
 flavor_name = var.flavor_name
