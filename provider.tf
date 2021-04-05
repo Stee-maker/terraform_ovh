a0c44831a8ebc54c4a01e77c159d9f4c45f57485
System Administrator <root@MacBook-Pro-de-Mac.local>
2021-04-05 16:52:16 +0200

    update provider v2

--- provider.tf
+++ provider.tf
@@ -3,11 +3,32 @@ required_providers {
 openstack = {
 source = "terraform-provider-openstack/openstack"
 version = "1.40.0"
+	} 
+
+ovh = {	
+source = "ovh/ovh"
+version = "0.11.0"
+	}
+
 } 
-} 
-  }
+ 
+backend "remote"{
+hostmane = "terraform.ovhcloud.tools"
+organizaeion = "tfe_workshop"
+
+workspaces {
+name = "terraform_ovh"
+	}
+}
+
+ }
+
 provider "openstack" {
 auth_url = "https://auth.cloud.ovh.net/v3/"
 alias = "ovh"
 region = var.region
   }
+
+provider "ovh" {
+	endpoint = "ovh.eu"
+	}
