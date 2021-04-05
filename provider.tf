91aed852961f8290321b58d383aadcd7777e9b08
System Administrator <root@MacBook-Pro-de-Mac.local>
2021-04-05 15:48:33 +0200

    update v1

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
 required_providers {
 openstack = {
-source = &quot;terraform-provider-openstack/openstack&quot;
-version = &quot;1.40.0&quot;
+source = "terraform-provider-openstack/openstack"
+version = "1.40.0"
 } }
 }
-provider &quot;openstack&quot; {
-auth_url = &quot;https://auth.cloud.ovh.net/v3/&quot;
-alias = &quot;ovh&quot;
+provider "openstack" {
+auth_url = "https://auth.cloud.ovh.net/v3/"
+alias = "ovh"
 region = var.region
--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
-resource "openstack_compute_keypair_v2" "gistools_keypair" {
+resource "openstack_compute_keypair_v2" "mysecret_key" {
 provider = openstack.ovh
-name = "gistools-pubkey"
-public_key = var.gistools_keypair
+name = "mysecret-pubkey"
+public_key = var.mysecret_key
 }
 resource "openstack_compute_instance_v2" "terraform_instance_demo" {
 provider = openstack.ovh
@@ -9,7 +9,7 @@ count = 1
 name = "terraform_instance_demo"
 image_name = var.image_name
 flavor_name = var.flavor_name
-key_pair = openstack_compute_keypair_v2.gistools_keypair.name
+key_pair = openstack_compute_keypair_v2.mysecret_key.name
 network {
 name = "Ext-Net"
 }
