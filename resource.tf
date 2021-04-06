f6b9ccc4e9cc368c560aa0a0d12c4f4fedfb4758
Stee-maker <59018335+Stee-maker@users.noreply.github.com>
2021-04-06 08:17:04 +0200

    Update resource.tf
    "GRA11"

--- resource.tf
+++ resource.tf
@@ -5,7 +5,7 @@ count = var.server_number
 name = "terraform_instance_demo"
 image_id = "fea018c5-c7de-4cb1-92a6-218bcdc47a95"
 flavor_id = "fa05492b-f252-4287-bf26-8bfa62933c6a"
-region = var.region
+region = "GRA11"
 network {
 name = "Ext-Net"
 }
