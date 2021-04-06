287e66fa6cf31ba103eed2cd248cf06bfa305848
Stee-maker <59018335+Stee-maker@users.noreply.github.com>
2021-04-06 08:02:33 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,15 +1,10 @@
-resource "openstack_compute_keypair_v2" "mysecret_key" {
-provider = openstack.ovh
-name = "mysecret_key"
-public_key = var.mysecret_key
-}
+
 resource "openstack_compute_instance_v2" "terraform_instance_demo" {
 provider = openstack.ovh
 count = var.server_number
 name = "terraform_instance_demo"
-image_name = var.image_name
-flavor_name = var.flavor_name
-key_pair = openstack_compute_keypair_v2.mysecret_key.name
+image_id = "fea018c5-c7de-4cb1-92a6-218bcdc47a95"
+flavor_id = "fa05492b-f252-4287-bf26-8bfa62933c6a"
 network {
 name = "Ext-Net"
 }
